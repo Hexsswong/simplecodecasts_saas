728bf4c1430d75afff8e12fc3cb878b53477b42d
hecticrail <hectorken@gmail.com>
2016-02-18 04:54:03 +0000

    Added profiles new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,9 @@
 class ProfilesController < ApplicationController
-    
+    def new
+        # form where a user can fill out their own profile.
+        @user = User.find( params[:user_id] )
+        # I can past the variable below to the new profile page
+        @variable = params[:hello]
+        @profile = @user.build_profile
+    end
 end
